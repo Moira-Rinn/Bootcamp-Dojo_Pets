e2b039eb7caaeb422461c067d0527865bd57a968
Sacha Moira Rinn <Moira.Rinn@gmail.com>
2021-08-13 03:52:05 -0400

    Added basic strucgture to classes, finished for the night.

---
+++ Dojo_Pets.py
@@ -0,0 +1,37 @@
+class Ninja:
+    def __init__(self, first_name, last_name, pet, treats, pet_food):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pet = pet
+        self.treats = treats
+        self.pet_food = pet_food
+
+    def walk(self):
+        pass
+
+    def feed(self):
+        pass
+
+    def bathe(self):
+        pass
+
+
+class Pet:
+    def __init__(Ninja, name, kind, tricks, health, energy):
+        self.name = name
+        self.kind = kind
+        self.tricks = tricks
+        self.health = health
+        self.energy = energy
+
+    def sleep():
+        pass
+
+    def eat():
+        pass
+
+    def play():
+        pass
+
+    def noise():
+        pass
